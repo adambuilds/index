0936314e32b73dbda21d8e2d78116bca5e150819
younga <adam.builds@outlook.com>
2025-03-16 03:53:40 -0400

    Remove irrelevant views, update views to use @extends instead of slots

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 [![Laravel Forge Site Deployment Status](https://img.shields.io/endpoint?url=https%3A%2F%2Fforge.laravel.com%2Fsite-badges%2F53949abd-a003-47ea-8abe-86c312a2b554%3Flabel%3D1%26commit%3D1&style=flat-square)](https://forge.laravel.com/servers/863298/sites/2542382)
 
-# index
+# index.one
 
 Development pathfinder for the **Index Integration Project**.
 
-Deployed to https://index.one, but intended to be portable. Maybe eventually even distributed.
+Deployed to https://index.one, but intended to be portable.
 
 ## Notes
 
 - dark mode is set via layout blade `<html class='dark'>` to not burn my eyes
+- use Inertia.js to avoid the dreaded page refresh?
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
 
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
+        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('My Subjects') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('Ensure your account is using a long, random password to stay secure.') }}
+        </p>
+    </header>
+
+    
+
+</section>
+
+            </div>
+        </div>
+
+    </div>
+</div>
 
 @endsection
