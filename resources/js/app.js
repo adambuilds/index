19f0a09101a97049be2b506b58cb4994e4c52ac0
Adam <adam@index.one>
2025-06-13 16:36:20 -0400

    Embed subject search Alpine component

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,27 +2,6 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
-window.subjectSearch = function () {
-    return {
-        query: '',
-        results: [],
-        selectedId: null,
-        search() {
-            if (this.query.length < 2) {
-                this.results = [];
-                return;
-            }
-            axios.get('/subject/search', { params: { q: this.query } })
-                .then(r => { this.results = r.data.subjects; });
-        },
-        select(item) {
-            this.query = item.name;
-            this.selectedId = item.id;
-            this.results = [];
-        },
-    };
-};
-
 window.Alpine = Alpine;
 
 Alpine.start();
--- resources/views/components/subject-search.blade.php
+++ resources/views/components/subject-search.blade.php
@@ -1,4 +1,4 @@
-<div x-data="subjectSearch()" class="relative">
+<div x-data="subjectSearch" class="relative">
     <x-text-input type="text" x-model="query" @input.debounce.300ms="search" placeholder="Search subjects..." class="mt-1 block w-full" autocomplete="off" />
     <input type="hidden" name="{{ $fieldName ?? 'parent_subject_id' }}" :value="selectedId" />
     <ul x-show="results.length" class="absolute z-10 bg-white border rounded w-full mt-1">
@@ -7,3 +7,26 @@
         </template>
     </ul>
 </div>
+
+<script>
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('subjectSearch', () => ({
+            query: '',
+            results: [],
+            selectedId: null,
+            search() {
+                if (this.query.length < 2) {
+                    this.results = [];
+                    return;
+                }
+                axios.get('/subject/search', { params: { q: this.query } })
+                    .then(r => { this.results = r.data.subjects; });
+            },
+            select(item) {
+                this.query = item.name;
+                this.selectedId = item.id;
+                this.results = [];
+            },
+        }));
+    });
+</script>
