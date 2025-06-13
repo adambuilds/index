b4da17f40c89f44de3b3d3f371b06eee26247729
Adam <adam@index.one>
2025-06-13 16:16:28 -0400

    Remove dashboard search field and fix Alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,17 +2,16 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
-
-Alpine.start();
-
 window.subjectSearch = function () {
     return {
         query: '',
         results: [],
         selectedId: null,
         search() {
-            if (this.query.length < 2) { this.results = []; return; }
+            if (this.query.length < 2) {
+                this.results = [];
+                return;
+            }
             axios.get('/subject/search', { params: { q: this.query } })
                 .then(r => { this.results = r.data.subjects; });
         },
@@ -20,6 +19,10 @@ window.subjectSearch = function () {
             this.query = item.name;
             this.selectedId = item.id;
             this.results = [];
-        }
-    }
-}
+        },
+    };
+};
+
+window.Alpine = Alpine;
+
+Alpine.start();
